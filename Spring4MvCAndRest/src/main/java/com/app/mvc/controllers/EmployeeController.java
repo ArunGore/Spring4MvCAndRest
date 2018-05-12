3d1a41f61cbc4aa245325b0caff18e236fd03cfc
Arun <Arun@192.168.0.4>
2018-05-12 16:11:55 +0530

    added views folder

--- Spring4MvCAndRest/src/main/java/com/app/mvc/controllers/EmployeeController.java
+++ Spring4MvCAndRest/src/main/java/com/app/mvc/controllers/EmployeeController.java
@@ -1,5 +1,16 @@
 package com.app.mvc.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/mvc/employee")
 public class EmployeeController {
 
+	@GetMapping("/getaddemployeepage")
+	public String getEmployeePage() {
+		return "employeeaddpage";
+	}
+	
 }
